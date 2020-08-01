ce00e319c98280aa02460621f2f47847c0e4d6e1
ciprian013 <sebastian.1701222550@ucaldas.edu.co>
2020-08-01 07:13:11 -0500

    Inicio de sesion arreglado basado en entity framework

---
+++ CiudadTatto/Controllers/LoginUsuarioController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CiudadTatto.Controllers
+{
+    public class LoginUsuarioController : Controller
+    {
+        // GET: LoginUsuario
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string User,string Password)
+        {
+            try
+            {   
+
+                using (Models.DB.TattooCityDBEntities1 db = new Models.DB.TattooCityDBEntities1())
+                {
+                    var oUser = (from d in db.usuario
+                                 where d.email == User && d.password == Password.Trim()
+                                 select d).FirstOrDefault();
+                    if(oUser == null)
+                    {
+                        ViewBag.Error = "Usuario o contraseña invalido";
+                        return View();
+
+                    }
+
+                    Session["User"] = oUser;
+
+
+                }
+
+                return RedirectToAction("Index", "Home");
+
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+
+                
+            }
+
+        }
+    }
+}
---
+++ CiudadTatto/Filters/VerificaSession.cs
@@ -0,0 +1,39 @@
+﻿using CiudadTatto.Controllers;
+using CiudadTatto.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CiudadTatto.Filters
+{
+    public class VerificaSession : ActionFilterAttribute
+    {
+        private usuario oUsuario;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+                oUsuario = (usuario)HttpContext.Current.Session["User"];
+                if (oUsuario == null)
+                {
+                    if (filterContext.Controller is LoginUsuarioController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/LoginUsuario/Login");
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+    }
+} 
